d7937a82ea70b5030f6912a09168aef2bca05afd
Javier F <144059587+djfarfan10@users.noreply.github.com>
2023-10-07 15:20:33 -0500

    Update Limpieza de base de datos.R

--- scripts/Limpieza de base de datos.R
+++ scripts/Limpieza de base de datos.R
@@ -26,27 +26,27 @@ df_test<- read.csv("test.csv")
 head(df_test)
 
 
-####### Limpieza de base de datos para el modelo de entrenamiento #######
+####### ---- Limpieza de base de datos para el modelo de entrenamiento ---- #######
 
 
-#Analizando la estructura de la base de datos
+### Analizando la estructura de la base de datos
+
 glimpse(df_train)
 
 df_train %>%
   count(property_type)
 
-##Verificación de NAs
+### Verificación de NAs
 
 sapply(df_train, function(x) sum(is.na(x)))
 
-## Dada la revisión , se elminan 9 entradas que no tienen description
+#Dada la revisión , se eliminan 9 entradas que no tienen description
 
 df_train_1<- df_train[!is.na(df_train$description), ]
 sapply(df_train_1, function(x) sum(is.na(x)))
 
-sapply(df_test, function(x) sum(is.na(x)))
 
-##Verificación del property_type
+### Verificación del property_type
 
 df_train_1 <- df_train_1 %>%
   mutate(property_type_2 = ifelse(grepl("casa", description), "casa", property_type))
@@ -56,8 +56,29 @@ df_train_1 <- df_train_1 %>%
   mutate(property_type_2 = ifelse(grepl("apto|apartamento", description), "apartamento", property_type_2)) %>%
   select(-property_type)
 
-## Se verifica si se puede obtener la variable área
+
+### Normalización de texto de la variable description
+
+# Todo el texto a minisculas
+df_train_1 <- df_train_1 %>%
+  mutate(description = str_to_lower(description))
+
+# Eliminación de tildes
+df_train_1 <- df_train_1 %>%
+  mutate(description = iconv(description, from = "UTF-8", to = "ASCII//TRANSLIT"))
+
+# Eliminamción de caracteres especiales
+df_train_1 <- df_train_1 %>%
+  mutate(description = str_replace_all(description, "[^[:alnum:]]", " "))
 
 
+# Eliminamos espacios extras
+df_train_1 <- df_train_1 %>%
+  mutate(description = str_trim(gsub("\\s+", " ", description)))
+
+#Creación de variable dicotoma para parqueaderos o garajes
+
+df_train_1 <- df_train_1 %>%
+  mutate(parqueadero = as.numeric(grepl("parqueadero|garaje", df_train_1$description)))
 
 
