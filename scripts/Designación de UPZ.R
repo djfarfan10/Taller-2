7a9580153d6626e2c9b91d65211baf1c677c1cbf
Andrés Felipe Díaz Barreto <af.diaz27@uniandes.edu.co>
2023-10-22 20:03:15 -0500

    Actualización

--- scripts/Designación de UPZ.R
+++ scripts/Designación de UPZ.R
@@ -1,5 +1,5 @@
 ################################################################################
-#              Obtención de UPZ                        #
+#                      Obtención de UPZ                                        #
 ################################################################################
 rm(list=ls())
 
@@ -18,21 +18,27 @@ p_load(tidyverse, # Manipular dataframes
        rattle, # Interfaz gráfica para el modelado de datos
        spatialsample) # Muestreo espacial para modelos de aprendizaje automático
 
+#Cargue de bases de datos
+
+train<-load()
+test<-load()
+
 UPZ <- st_read("C:/Users/afdia/OneDrive - Universidad de los Andes/Maestría en Economía Aplicada/Big Data y Machine Learning/Repositorios-GitHub/Taller-2/stores/poblacion-upz-bogota.geojson")
 
 #Renombrar las columnas de interés
 colnames(UPZ)[2]<-"Localidad"
 colnames(UPZ)[4]<-"UPZ"
 
-
 #Transformación del sistema de proyección de UPL
 
 train <- st_as_sf(train, coords = c("lon", "lat"), crs=4326)
 test <- st_as_sf(test, coords = c("lon", "lat"), crs=4326)
 
-UPL<-st_transform(UPL, crs=st_crs(train))
+UPZ<-st_transform(UPL, crs=st_crs(train))
 
 #Unión de las bases de datos
 
-train<-st_join(train, UPL, join=st_intersects)
-test<-st_join(test, UPL, join=st_intersects)
+train<-st_join(train, UPZ, join=st_intersects)
+test<-st_join(test, UPZ, join=st_intersects)
+
+#
