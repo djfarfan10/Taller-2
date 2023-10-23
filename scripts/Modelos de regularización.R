57b16835c6629212b8e628ea7cf3480ae46f0884
Andrés Felipe Díaz Barreto <af.diaz27@uniandes.edu.co>
2023-10-23 14:05:08 -0500

    Update Modelos de regularización.R

--- scripts/Modelos de regularización.R
+++ scripts/Modelos de regularización.R
@@ -7,14 +7,38 @@ p_load("tidyverse","stargazer","glmnet","rio","tidymodels")
 
 #Cargue de bases de datos
 
-train<-load()
-test<-load()
+load("C:/Users/afdia/OneDrive - Universidad de los Andes/Maestría en Economía Aplicada/Big Data y Machine Learning/Repositorios-GitHub/Taller-2/stores/train_def1.Rda")
+load("C:/Users/afdia/OneDrive - Universidad de los Andes/Maestría en Economía Aplicada/Big Data y Machine Learning/Repositorios-GitHub/Taller-2/stores/test_def1.Rda")
+train<-df_train_merged
+test<-df_test_merged
+
+train<-train %>% select(-geometry,-geo_point_2d)
+test<-test %>% select(-geometry)
+
+#Valoración de baño social
+
+train <- train %>% mutate(bano_social = case_when(bano_social == "si"~ 1,
+                                                  bano_social == "no"~ 0))
+
+
+test <- test %>% mutate(bano_social = case_when(bano_social == "si"~ 1,
+                                                  bano_social == "no"~ 0))
+                    
+
+#Definición de variables categóricas
+variables_categoricas <- c("parqueadero",
+                            "bano_social",
+                            "deposito_def",
+                            "estado_construccion",
+                            "estado_remodelado", 
+                            "terraza_balcon_def",
+                            "estrato",
+                            "cod_loc",
+                            "cod_upz")
 
 
 ridge_spec <- linear_reg(mixture = 0, penalty = 0) %>%
-  # Establecer el modo del modelo como 'regresión'
   set_mode("regression") %>%
-  # Configurar el motor del modelo como 'glmnet', que se utiliza para modelos ridge  y lasso
   set_engine("glmnet")
 
 #Definición de variable a predecir
